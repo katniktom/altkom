f6e58cd031b1fb358c083b7b59cfde1d1912f6a5
5 <5@5-Komputer.319.wroclaw.altkom.local>
2014-07-03 16:01:52 +0200

    v14

--- src/pl/altkom/Start.java
+++ src/pl/altkom/Start.java
@@ -5,11 +5,19 @@ import java.util.Scanner;
 public class Start {
 
 	public static void main(String[] args) {
-		String odczyt = "fjgd";
+				
+			 Scanner sc = new Scanner(System.in);
+		     int i = sc.nextInt();
+		switch (i) {
+		case 1:
+				System.out.print(i+"hahaha");
+				break;
+		case 2:
+			System.out.print(i+"hahaha" + i + i );
+			break;
+default:
+			System.out.print("xxxx");
+		}
 		
-		Scanner odczyt1 = new Scanner(System.in);
-		
-				System.out.println(odczyt1);		
+		}
 	}
-
-}
