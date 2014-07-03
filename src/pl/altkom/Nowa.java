96f7452672dc46c474c937f4b623871aea7acdb5
5 <5@5-Komputer.319.wroclaw.altkom.local>
2014-07-03 13:02:13 +0200

    v12

--- src/pl/altkom/Nowa.java
+++ src/pl/altkom/Nowa.java
@@ -5,6 +5,7 @@ public class Nowa {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("dzień niedobry World");
+System.out.println("dzień dobry World");
 	}
 
 }
